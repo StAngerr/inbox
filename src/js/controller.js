f81499ecfccf1677655b81aabc7722ed9a874e8b
Taras Stekhnovych <violet.infierno@gmail.com>
2015-02-27 17:40:45 +0200

    Perfect pixel corrections.

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,17 +2,32 @@ var inbox=angular.module('inbox',[]);
 
 inbox.controller('TasksCtrl', function($scope,$http) {
 
+	/*SEARCH event*/
+	$scope.searchAnimationEvent = function() {
+		/*$('#searchField')*/
+/*			$('#searchField').css({'visibility' : 'visible'});*/
+		$('#searchField').css({'visibility' : 'visible'}).addClass('.borderAnim');
+		$('#searchField').focus();
+	};
+	/*   */
+
 	$http.get('src/content/tasks.json').success(function(data, status, headers, config) {
 		 $scope.tasks = data;
 	});
 
 	$scope.openComment = function(event) {
 		var element =  event.currentTarget;
+		
+		if () {
+			
+		}
 
 		if( $(window).width() < 620) {
 			document.getElementById('navigation').style.marginLeft = '-50%';
 		}
+
 		$('.taskItem').addClass('activeTask');
+
 		$(element).removeClass('activeTask');
 	};
 

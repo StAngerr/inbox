2763f33e4ea4764ae9cbb43eb0e45ac64a20258d
Taras Stekhnovych <violet.infierno@gmail.com>
2015-03-25 15:25:55 +0200

    Drop down menu in opened task restyled.

--- src/js/events.js
+++ src/js/events.js
@@ -3,11 +3,3 @@ $(window).resize(function() {
 		$('*').removeClass('slideLeft');
 	}
 });
-
-$(document).on('click', function(event) {
-	if( $(event.target).hasClass('taskMenu') || $(event.target.offsetParent).hasClass('taskMenu')) {
-
-	}else {
-		if($('.dropDownMenu').is(':visible')) $('.dropDownMenu').hide(300);
-	}
-});
